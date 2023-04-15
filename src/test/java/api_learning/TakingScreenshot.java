ea6d3f62f00672963888c32923c9ba5927efbaf7
helennguyen30687 <helennguyen30687@gmail.com>
2023-04-15 22:36:09 +0700

    Lession 17, taking screenshot

---
+++ src/test/java/api_learning/TakingScreenshot.java
@@ -0,0 +1,46 @@
+package api_learning;
+
+import driver.DriverFactory;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.MobileElement;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import platform.Platform;
+
+import java.io.File;
+
+public class TakingScreenshot {
+    public static void main(String[] args) {
+        AppiumDriver<MobileElement> appiumDriver = DriverFactory.getDriver(Platform.ANDROID);
+
+        try {
+            MobileElement navLoginScreenBtnElem = appiumDriver.findElement(MobileBy.AccessibilityId("Login"));
+            navLoginScreenBtnElem.click();
+            WebDriverWait wait = new WebDriverWait(appiumDriver,5);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(MobileBy.AccessibilityId("Login")));
+
+            //Whole screen
+            File base64ScreenshotData = appiumDriver.getScreenshotAs(OutputType.FILE);
+            String fileLocation = System.getProperty("user.dir").concat("/screenshots/").concat("LoginScreen.png");
+            FileUtils.copyFile(base64ScreenshotData,new File(fileLocation));
+
+            //An area
+            MobileElement loginFormElem = appiumDriver.findElement(MobileBy.AccessibilityId("Login-screen"));
+            base64ScreenshotData = loginFormElem.getScreenshotAs(OutputType.FILE);
+            fileLocation = System.getProperty("user.dir").concat("/screenshots/").concat("LoginForm.png");
+            FileUtils.copyFile(base64ScreenshotData,new File(fileLocation));
+
+            //An Element
+            MobileElement loginBtnElem = appiumDriver.findElement(MobileBy.AccessibilityId("button-LOGIN"));
+            base64ScreenshotData = loginBtnElem.getScreenshotAs(OutputType.FILE);
+            fileLocation = System.getProperty("user.dir").concat("/screenshots/").concat("LoginButton.png");
+            FileUtils.copyFile(base64ScreenshotData,new File(fileLocation));
+        } catch (Exception e) {
+
+        }
+    }
+}
